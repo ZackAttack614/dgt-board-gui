30803ece2af96f8c747111d46ad350a63a17e773
Kevin Lewis <kevinlewis32@gmail.com>
2023-04-10 18:07:10 -0400

    Eval bar

--- src/Board.js
+++ src/Board.js
@@ -35,10 +35,42 @@ class ChessBoard extends React.Component {
     this.pieces = {};
 
     this.state = { fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR' };
-
+    this.lastFen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR';
+    this.currentMove = 'w'; 
+    this.castling = {
+      'w': {
+        'k': true,
+        'q': true,
+      },
+      'b': {
+        'k': true,
+        'q': true,
+      }
+    }
+    this.enPassant = null;
     // Using an object so it can be a hashmap
     this.arrows = {};
     this.clearArrowsOnMove = true;
+    this.evaluation = 0;
+
+    // Connect to the websocket for stockfish evaluation
+    this.ws = new WebSocket("ws://localhost:8080");
+    // When the websocket is open, send the initial position
+    this.ws.onopen = () => {
+      this.ws.send("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    }
+    // When the websocket receives a message, update the evaluation
+    this.ws.onmessage = (event) => {
+      this.evaluation = parseInt(event.data);
+      // If it's black's move, negate the evaluation
+      if (this.currentMove === 'b') {
+        this.evaluation = -this.evaluation;
+      }
+      // Log the evaluation
+      console.log("Evaluation: ", this.evaluation/ 100.0);
+      // Force a re-render 
+      this.forceUpdate();
+    }
   }
 
   componentDidMount() {
@@ -52,12 +84,133 @@ class ChessBoard extends React.Component {
       const param = JSON.parse(event.data).param;
       if (param) {
         if (param.board !== null && param.board !== undefined) {
+           // Check if there was no change
+          if (this.lastFen === param.board)
+            return;
+          // Clear the arrows if the clearArrowsOnMove flag is set
+          if (this.clearArrowsOnMove) {
+            this.arrows = {};
+
+          }
+
+
           this.setState({ fen: param.board });
+         
+          
+          
+          
+          // See what piece has moved
+          let lastBoard = this.lastFen.split('/').map(row => row.split(''));
+          let board = param.board.split('/').map(row => row.split(''));
+          // FEN is read from bottom to top, so reverse the board
+          lastBoard.reverse();
+          board.reverse();
+          // Replace lastFen with the new fen
+          this.lastFen = param.board;
+
+
+          // Parse each row to make the board a 2d array
+          for (let i = 0; i < 8; i++) {
+            // Replace the numbers with empty strings
+            for (let j = 0; j < 8; j++) {
+              if (lastBoard[i][j] >= '1' && lastBoard[i][j] <= '8') {
+                let num = parseInt(lastBoard[i][j]);
+                lastBoard[i].splice(j, num, ...Array(num).fill(''));
+              }
+              if (board[i][j] >= '1' && board[i][j] <= '8') {
+                let num = parseInt(board[i][j]);
+                board[i].splice(j, num, ...Array(num).fill(''));
+              }
+            }
+          }
+          
+          let movedPiece = null;
+          let movedFrom = null;
+          let movedTo = null;
+          for (let i = 0; i < 8 && movedPiece == null; i++) {
+            for (let j = 0; j < 8; j++) {
+              if (board[i][j] !== lastBoard[i][j]) {
+                movedPiece = board[i][j];
+                movedFrom = [j, i];
+                movedTo = [j, i];
+                break;
+              }
+            }
+          }
+          // Check if the piece is upper case
+          if (movedPiece === movedPiece.toUpperCase())
+            // This means it was a white piece, so set the current move to black
+            this.currentMove = 'b';
+          else
+            // This means it was a black piece, so set the current move to white
+            this.currentMove = 'w';
+          // Check if the piece is a pawn that moved two squares
+          if (movedPiece === 'p' && movedFrom[1] === 6 && movedTo[1] === 4) {
+            // This means it was a black pawn that moved two squares, so set the en passant square
+            this.enPassant = [movedTo[0], movedTo[1] + 1];
+          } else if (movedPiece === 'P' && movedFrom[1] === 1 && movedTo[1] === 3) {
+            // This means it was a white pawn that moved two squares, so set the en passant square
+            this.enPassant = [movedTo[0], movedTo[1] - 1];
+          } else {
+            // This means it was not a pawn that moved two squares, so remove the en passant square
+            this.enPassant = null;
+          }
+          // Change en passant array to a string
+          if (this.enPassant !== null) {
+            // Get the square as a string
+            this.enPassant = String.fromCharCode(this.enPassant[0] + 65) + (this.enPassant[1] + 1);
+          }
+
+          // Check if the piece is a king
+          if (movedPiece === 'k') {
+            // This means it was a black king, so remove the black king castling rights
+            this.castling['b']['k'] = false;
+            this.castling['b']['q'] = false;
+          } else if (movedPiece === 'K') {
+            // This means it was a white king, so remove the white king castling rights
+            this.castling['w']['k'] = false;
+            this.castling['w']['q'] = false;
+          }
+
+          // Check if the piece is a rook
+          if (movedPiece === 'r') {
+            // This means it was a black rook, so check if it was the left or right rook
+            if (movedFrom[0] === 0) {
+              // This means it was the left rook, so remove the black queen side castling rights
+              this.castling['b']['q'] = false;
+            } else if (movedFrom[0] === 7) {
+              // This means it was the right rook, so remove the black king side castling rights
+              this.castling['b']['k'] = false;
+            }
+          } else if (movedPiece === 'R') {
+            // This means it was a white rook, so check if it was the left or right rook
+            if (movedFrom[0] === 0) {
+              // This means it was the left rook, so remove the white queen side castling rights
+              this.castling['w']['q'] = false;
+            } else if (movedFrom[0] === 7) {
+              // This means it was the right rook, so remove the white king side castling rights
+              this.castling['w']['k'] = false;
+            }
+          }
+
+          // Send a message to the server for evaluation purposes
+          // Create an actual fen
+          let fen = param.board + ' ' + this.currentMove + ' ' + 
+            (this.castling['w']['k'] ? 'K' : '') + 
+            (this.castling['w']['q'] ? 'Q' : '') + 
+            (this.castling['b']['k'] ? 'k' : '') + 
+            (this.castling['b']['q'] ? 'q' : '') + ' ' + 
+            (this.enPassant ? this.enPassant : '') + ' - 0 1';
+
+          this.ws.send(fen);
+
           this.updatePosition();
-        }
+          }
       }
-    });
+  });
   }
+
+
 
   render() {
     this.updatePosition();
@@ -85,6 +238,8 @@ class ChessBoard extends React.Component {
     // Optional: Add subtle shading to the squares
     const scarletShadow = "inset 0 0 5px rgba(0, 0, 0, 0.2)";
     const grayShadow = "inset 0 0 5px rgba(0, 0, 0, 0.2)";
+
+    
 
     return (
       <div onContextMenu={(e) => {
@@ -201,6 +356,9 @@ class ChessBoard extends React.Component {
 
       }
       style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <div id = "eval-bar" style = {{height: cellSize * boardSize + 2}}>
+          <div id = "eval-white" style = {{height: `${ 100/(1 + Math.exp(-this.evaluation / 300.0)) }%`}}><p class = {this.evaluation >= 0 ? 'white' : 'black' }>{Math.abs((this.evaluation / 100.0)).toFixed(1)}</p></div>
+        </div>
         <div style={{ position: "relative" }}>
         <canvas id = "arrow-canvas" width = {cellSize * boardSize} height = {cellSize * boardSize} style = {{position:"absolute", top:1, left: 1, zIndex:1000, pointerEvents:'none'}}></canvas>
           <div style={{ position: "absolute", bottom: -notationSize, left: 0, color: white }}>
@@ -239,10 +397,6 @@ class ChessBoard extends React.Component {
   updatePosition() {
     // Clear the previous pieces from the array.
     this.pieces = [];
-    // Clear the arrows
-    if (this.clearArrowsOnMove) {
-      this.arrows = {};
-    };
 
     // Parse the FEN to update the component's piece tracking
     for (let row = 0; row < 8; row++) {
